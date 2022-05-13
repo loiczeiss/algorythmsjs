a24db611792a2d37deecbd3916ebe5a39b44612d
LoicZeiss <loiczeiss@gmail.com>
2022-05-13 16:19:53 +0200

    exos done

--- var a = 2.js
+++ var a = 2.js
@@ -24,5 +24,34 @@ var b =5
 console.log(modTwoNum(a,b))*/
 
 //concatenate sentence//
+/*var a =  "Hello, I'm lucca"
+var b = " what about you?"
+        function contxt (a,b) {
+            return a+b
+        }
+        console.log(contxt(a,b))*/
 
+//TVA//
+/*var a = 100
+var b = a*0.21
+        function TVA (a) {
+            return a + b
+        }
+console.log(TVA(a,b))*/
 
+//surface of a circle
+/*var a = 21
+var b = Math.PI
+        function Circle (a,b) {
+            return a*a*b
+        }
+console.log(Circle(a,b))*/
+
+//Conversion time to seconds
+/*var a = 18
+var b = 45 
+var c = 37
+  function time(a,b,c) {
+      return ((a*60*60)+(b*60)+c)
+  }
+console.log(time(a,b,c))*/
