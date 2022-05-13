be98c0555689f500683421bd38e0d1f6c3869c02
LoicZeiss <loiczeiss@gmail.com>
2022-05-13 16:01:10 +0200

    var debut

---
+++ var a = 2.js
@@ -0,0 +1,28 @@
+//algo sum 2 numbers//
+/*var a = 2
+var b = 3
+ function sumTwoNum (a,b) {
+     return  a+b+
+ }
+
+console.log(sumTwoNum(a, b))*/
+
+//algo div 2 numbers//
+/*var a = 5
+var b =  5
+    function Div (a, b) {
+        return a/b
+    }
+console.log(Div(a, b))*/
+
+//modulo two var//
+/*var a = 18
+var b =5 
+        function modTwoNum (a,b) {
+            return a%b
+        }
+console.log(modTwoNum(a,b))*/
+
+//concatenate sentence//
+
+
